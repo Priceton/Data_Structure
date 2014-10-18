9e5ef4b713e0d79976f2bbae6bb2d79bd6587b8f
Priceton <mypriceton@gmail.com>
2014-10-18 22:57:28 +0800

    2ed version

--- String/SimplifySting.cpp
+++ String/SimplifySting.cpp
@@ -13,7 +13,7 @@ void SimplifyString(char str[], int len){
 	int index = 0;
 	for(int i= iStart+1; i<len; ++i){
 		if(str[i] != ' ' || str[i-1] != ' '){
-			str[++index] = str[i];
+			str[index++] = str[i];
 		}
 	}
 	str[index] = '\0';
